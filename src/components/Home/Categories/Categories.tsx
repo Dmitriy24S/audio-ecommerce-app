5d7ed5ecf22554570978c4b858c3a707a784af09
Dmitriy24S <devilins24@gmail.com>
2022-12-22 14:10:31 +0200

    refactor: Categories Links: use links.tsx for data

--- src/components/Home/Categories/Categories.tsx
+++ src/components/Home/Categories/Categories.tsx
@@ -1,74 +1,36 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { links } from '../../../data/links'
 import styles from './Categories.module.css'
 
 const Categories = () => {
   return (
     <section className={styles.categories}>
-      <Link href='headphones' className={styles.card}>
-        <Image
-          src='/assets/shared/desktop/image-headphones.png'
-          alt='headphones'
-          className={styles.image}
-          width={155}
-          height={145}
-        />
-        <div className={styles.content}>
-          <h4 className={styles.title}>Headphones</h4>
-          <div className={styles.button}>
-            Shop
-            <Image
-              src='/assets/shared/desktop/icon-arrow-right.svg'
-              alt='arrow right'
-              width={12}
-              height={16}
-            />
+      {/* exlude Home (index 0) link for categories */}
+      {links.slice(1).map((link) => (
+        <Link href={link.url} className={styles.card} key={link.id}>
+          <Image
+            src={link.img}
+            alt={link.text}
+            className={styles.image}
+            width={155}
+            height={145}
+          />
+          <div className={styles.content}>
+            <h4 className={styles.title}>{link.text}</h4>
+            <div className={styles.button}>
+              Shop
+              <Image
+                src='/assets/shared/desktop/icon-arrow-right.svg'
+                alt='arrow right'
+                width={12}
+                height={16}
+              />
+            </div>
           </div>
-        </div>
-      </Link>
-      <Link href='speakers' className={styles.card}>
-        <Image
-          src='/assets/shared/desktop/image-speakers.png'
-          alt='speakers'
-          className={styles.image}
-          width={158}
-          height={145}
-        />
-        <div className={styles.content}>
-          <h4 className={styles.title}>Speakers</h4>
-          <div className={styles.button}>
-            Shop
-            <Image
-              src='/assets/shared/desktop/icon-arrow-right.svg'
-              alt='arrow right'
-              width={12}
-              height={16}
-            />
-          </div>
-        </div>
-      </Link>
-      <Link href='earphones' className={styles.card}>
-        <Image
-          src='/assets/shared/desktop/image-earphones.png'
-          alt='earphones'
-          className={styles.image}
-          width={155}
-          height={145}
-        />
-        <div className={styles.content}>
-          <h4 className={styles.title}>Earphones</h4>
-          <div className={styles.button}>
-            Shop
-            <Image
-              src='/assets/shared/desktop/icon-arrow-right.svg'
-              alt='arrow right'
-              width={12}
-              height={16}
-            />
-          </div>
-        </div>
-      </Link>
+        </Link>
+      ))}
     </section>
   )
 }
---
+++ src/data/links.tsx
@@ -0,0 +1,26 @@
+export const links = [
+  {
+    id: 1,
+    text: 'home',
+    url: '/',
+    img: '',
+  },
+  {
+    id: 2,
+    text: 'Headphones',
+    url: '/headphones',
+    img: '/assets/shared/desktop/image-headphones.png',
+  },
+  {
+    id: 3,
+    text: 'Speakers',
+    url: '/speakers',
+    img: '/assets/shared/desktop/image-speakers.png',
+  },
+  {
+    id: 4,
+    text: 'Earphones',
+    url: '/earphones',
+    img: '/assets/shared/desktop/image-earphones.png',
+  },
+]
