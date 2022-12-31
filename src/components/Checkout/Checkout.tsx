46ed132caa651b34310f744ed3bd3cb5d17f03d1
Dmitriy24S <devilins24@gmail.com>
2022-12-31 19:42:53 +0200

    feat: add React Hook Form w/ validation

--- src/components/Checkout/Checkout.tsx
+++ src/components/Checkout/Checkout.tsx
@@ -1,13 +1,63 @@
+import { yupResolver } from '@hookform/resolvers/yup'
 import React from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import * as yup from 'yup'
 import CheckoutForm from '../CheckoutForm/CheckoutForm'
 import CheckoutSummary from '../CheckoutSummary/CheckoutSummary'
 import styles from './Checkout.module.css'
 
+export interface IFormInputs {
+  name: string
+  email: string
+  phone: string
+  address: string
+  zip: string
+  city: string
+  country: string
+  payment: string
+  eMoney: string
+  eMoneyPin: string
+}
+
+export const schema = yup
+  .object({
+    name: yup.string().min(2).required(),
+    email: yup.string().email().required(),
+    phone: yup.string().min(8).required(),
+    address: yup.string().min(4).required(),
+    zip: yup.string().min(2).required(),
+    city: yup.string().required(),
+    country: yup.string().required(),
+    payment: yup.string().required(),
+    eMoney: yup.string().min(8).required(),
+    eMoneyPin: yup.string().min(4).required(),
+  })
+  .required()
+
 const Checkout = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<IFormInputs>({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      payment: '', // prevent null from radio select?
+    },
+  })
+
+  const onSubmit: SubmitHandler<IFormInputs> = (data) => {
+    console.log('data:', data)
+  }
+
+  const watchAllFields = watch() // when pass nothing as argument, you are watching everything
+  console.log(watchAllFields)
+
   return (
-    <form className={styles.checkoutForm}>
+    <form className={styles.checkoutForm} onSubmit={handleSubmit(onSubmit)}>
       <h2>Checkout</h2>
-      <CheckoutForm />
+      <CheckoutForm register={register} errors={errors} />
       <CheckoutSummary />
     </form>
   )
--- src/components/CheckoutForm/CheckoutForm.tsx
+++ src/components/CheckoutForm/CheckoutForm.tsx
@@ -1,41 +1,82 @@
 import React from 'react'
+import { FieldErrors, UseFormRegister } from 'react-hook-form'
+import { IFormInputs } from '../Checkout/Checkout'
 import styles from './CheckoutForm.module.css'
 
-const CheckoutForm = () => {
+interface IProps {
+  register: UseFormRegister<IFormInputs>
+  errors: Partial<FieldErrors<IFormInputs>>
+}
+
+const CheckoutForm = ({ register, errors }: IProps) => {
+  //   {
+  //     "name": "fddfdf",
+  //     "email": "",
+  //     "phone": "",
+  //     "address": "",
+  //     "zip": "",
+  //     "city": "",
+  //     "country": "",
+  //     "payment": "e-Money",
+  //     "eMoney": "322342",
+  //     "eMoneyPin": "234234234"
+  // }
+
   return (
     <div className={styles.formFields}>
       <fieldset className={styles.billing}>
         <legend> Billing Details</legend>
         <div className={styles.inputContainer}>
           <label htmlFor='name'>Name</label>
-          <input type='text' name='name' id='name' />
+          <input type='text' id='name' {...register('name')} />
+          <p role='alert' className={styles.inputErrorMsg}>
+            {errors.name?.message}
+          </p>
         </div>
         <div className={styles.inputContainer}>
           <label htmlFor='email'>Email Address</label>
-          <input type='text' name='email' id='email' />
+          <input type='text' id='email' {...register('email')} />
+          <p role='alert' className={styles.inputErrorMsg}>
+            {errors.email?.message}
+          </p>
         </div>
         <div className={styles.inputContainer}>
-          <label htmlFor='phone'>Phone number</label>
-          <input type='text' name='phone' id='phone' />
+          <label htmlFor='phone'>Phone Number</label>
+          <input type='number' id='phone' {...register('phone')} />
+          <p role='alert' className={styles.inputErrorMsg}>
+            {errors.phone?.message}
+          </p>
         </div>
       </fieldset>
       <fieldset className={styles.shipping}>
         <legend>Shipping Info</legend>
         <div className={styles.inputContainer}>
           <label htmlFor='address'>Your Address</label>
-          <input type='text' name='address' id='address' />
+          <input type='text' id='address' {...register('address')} />
+          <p role='alert' className={styles.inputErrorMsg}>
+            {errors.address?.message}
+          </p>
         </div>
         <div className={styles.inputContainer}>
           <label htmlFor='zip'>Zip Code</label>
-          <input type='text' name='zip' id='zip' />
+          <input type='text' id='zip' {...register('zip')} />
+          <p role='alert' className={styles.inputErrorMsg}>
+            {errors.zip?.message}
+          </p>
         </div>
         <div className={styles.inputContainer}>
           <label htmlFor='city'>City</label>
-          <input type='text' name='city' id='city' />
+          <input type='text' id='city' {...register('city')} />
+          <p role='alert' className={styles.inputErrorMsg}>
+            {errors.city?.message}
+          </p>
         </div>
         <div className={styles.inputContainer}>
           <label htmlFor='country'>Country</label>
-          <input type='text' name='country' id='country' />
+          <input type='text' id='country' {...register('country')} />
+          <p role='alert' className={styles.inputErrorMsg}>
+            {errors.country?.message}
+          </p>
         </div>
       </fieldset>
       <fieldset className={styles.payment}>
@@ -43,30 +84,39 @@ const CheckoutForm = () => {
         <div className={styles.radioInputContainer}>
           <input
             type='radio'
-            name='payment'
             id='payment1'
             value='e-Money'
             title='e-Money'
+            {...register('payment')}
           />
           <label htmlFor='payment1'>e-Money</label>
         </div>
         <div className={styles.radioInputContainer}>
           <input
             type='radio'
-            name='payment'
+            {...register('payment')}
             id='payment2'
             value='Cash on Delivery'
             title='Cash on Delivery'
           />
           <label htmlFor='payment2'>Cash on Delivery</label>
+          <p role='alert' className={styles.inputErrorMsg}>
+            {errors.payment?.message}
+          </p>
         </div>
         <div className={styles.inputContainer}>
-          <label htmlFor='eMoneyNumber'>e-Money Number</label>
-          <input type='number' name='eMoneyNumber' id='eMoneyNumber' />
+          <label htmlFor='eMoney'>e-Money Number</label>
+          <input type='number' id='eMoney' {...register('eMoney')} />
+          <p role='alert' className={styles.inputErrorMsg}>
+            {errors.eMoney?.message}
+          </p>
         </div>
         <div className={styles.inputContainer}>
           <label htmlFor='eMoneyPin'>e-Money PIN</label>
-          <input type='number' name='eMoneyPin' id='eMoneyPin' />
+          <input type='number' id='eMoneyPin' {...register('eMoneyPin')} />
+          <p role='alert' className={styles.inputErrorMsg}>
+            {errors.eMoneyPin?.message}
+          </p>
         </div>
       </fieldset>
     </div>
